142dbbfcd8879e5e690271a1a8ec654b01e1b781
julioxbs@gmail.com <julioxbs@gmail.com>
2022-01-05 16:50:48 +0000

    first commit

---
+++ src/components/PollList.svelte
@@ -0,0 +1,22 @@
+<script>
+    import {fade, slide, scale} from "svelte/transition";
+    import { flip } from "svelte/animate";
+    import PollStore from "../stores/PollStore";
+    import PollDetails from "./PollDetails.svelte";
+</script>
+
+<div class="poll-list">
+    {#each $PollStore as poll (poll.id)}
+        <div transition:fade|local animate:flip={{duration: 500}}>
+            <PollDetails poll={poll} />
+        </div>
+    {/each}
+</div>
+
+<style>
+    .poll-list {
+        display: grid;
+        grid-template-columns: repeat(2, 1fr);
+        gap: 20px;
+    }
+</style>
